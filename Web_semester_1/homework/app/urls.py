bb7cfc8ad686f52db86858bef1e9302c0cdea3c0
Lam0Rka <sheremetev.bogdan2002@mail.ru>
2024-04-03 03:32:35 +0300

    make HW2

---
+++ Web_semester_1/homework/app/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from app import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('', views.paginate, name='pagination'),
+    path('hot/', views.hot, name='hot'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
+    path('settings/', views.settings, name='settings'),
+    path('signup/', views.signup, name='signup')
+]
